0f138e246b9f1068a4c3282f92d489c66f25f388
Oleg Khryptul <okreptul@yahoo.com>
2018-10-22 23:47:15 +0200

    Add colored diff output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use colored::*;
 use failure::{Error, ResultExt};
 use rayon::prelude::*;
 use regex::Regex;
+use std::borrow::Cow::*;
 use std::fs;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
@@ -14,6 +16,9 @@ struct Opt {
     /// Pattern string (rust regex).
     #[structopt(parse(from_str))]
     pattern: String,
+    /// Show replaced or matched lines.
+    #[structopt(short = "d", long = "show-diff")]
+    show_diff: bool,
     /// Replacement string (rust regex). Do only pattern matching if not specified.
     #[structopt(short = "r", long = "replace")]
     replacement: Option<String>,
@@ -22,22 +27,55 @@ struct Opt {
     input: Option<PathBuf>,
 }
 
-fn patch_file(path: &Path, re: &Regex, replacement: &Option<String>) -> Result<usize, Error> {
+fn print_diff(left: &str, right: &str) {
+    for diff in diff::lines(left, right) {
+        match diff {
+            diff::Result::Left(l) => println!("-{}", l.red()),
+            diff::Result::Right(r) => println!("+{}", r.green()),
+            _ => {}
+        }
+    }
+}
+
+fn patch_file(
+    path: &Path,
+    re: &Regex,
+    replacement: &Option<String>,
+    show_diff: bool,
+) -> Result<usize, Error> {
     let old_bytes = fs::read(path).context(format!("Cannot read from the file"))?;
     let old_text = String::from_utf8_lossy(&old_bytes);
 
-    if re.is_match(&old_text) {
-        if let Some(replace_str) = replacement {
-            let new_text = re.replace_all(&old_text, replace_str.as_str());
+    if let Some(replace_str) = replacement {
+        let new_text = re.replace_all(&old_text, replace_str.as_str());
 
-            fs::write(path, new_text.as_bytes())?;
+        if let Owned(s) = new_text {
+            println!("Changes in file: {:?}", path);
 
-            println!("File changed: {:?}", path);
-        } else {
-            println!("File matched: {:?}", path);
+            fs::write(path, s.as_bytes())?;
+
+            if show_diff {
+                print_diff(&old_text, &s);
+            }
+
+            return Ok(1);
+        }
+    } else {
+        println!("Matches in file: {:?}", path);
+
+        let mut matches_count = 0;
+        for pos in re.find_iter(&old_text) {
+            if show_diff {
+                println!("{}", pos.as_str().red());
+            }
+
+            matches_count += 1;
         }
 
-        return Ok(1);
+        if matches_count > 0 {
+            println!("Matches count: {}", matches_count);
+            return Ok(1);
+        }
     }
     Ok(0)
 }
@@ -46,6 +84,7 @@ fn patch_dir<P: AsRef<Path>>(
     path: P,
     re: &Regex,
     replacement: &Option<String>,
+    show_diff: bool,
 ) -> Result<usize, Error> {
     // NOTE: walkdir doesn't support rayon so we just collect file paths and then use rayon to process them in parallel
     let mut files = vec![];
@@ -54,15 +93,14 @@ fn patch_dir<P: AsRef<Path>>(
             if let Some(path_str) = entry.path().to_str() {
                 files.push(path_str.to_string());
             }
-            // patch_file(entry.path(), re, replacement)
-            //     .context(format!("File: {:?}", entry.path()))?;
         }
     }
 
     let files_processed = files
         .par_iter()
         .map(|file| {
-            patch_file(Path::new(file), re, replacement).context(format!("File: {:?}", file))
+            patch_file(Path::new(file), re, replacement, show_diff)
+                .context(format!("File: {:?}", file))
         })
         .inspect(|result| {
             if let Err(err) = result {
@@ -83,9 +121,10 @@ fn main() -> Result<(), Error> {
         let path = path_buf.as_path().clone();
         if path.exists() {
             if path.is_file() {
-                patch_file(path, &re, &opt.replacement).context(format!("File: {:?}", path))?
+                patch_file(path, &re, &opt.replacement, opt.show_diff)
+                    .context(format!("File: {:?}", path))?
             } else if path.is_dir() {
-                patch_dir(path, &re, &opt.replacement)?
+                patch_dir(path, &re, &opt.replacement, opt.show_diff)?
             } else {
                 eprintln!("Unknown type of the file {:?}", path);
                 0
@@ -95,10 +134,14 @@ fn main() -> Result<(), Error> {
             0
         }
     } else {
-        patch_dir("./", &re, &opt.replacement)?
+        patch_dir("./", &re, &opt.replacement, opt.show_diff)?
     };
 
-    println!("Total files: {}", files_processed);
+    if opt.replacement.is_some() {
+        println!("Total replaced files: {}", files_processed);
+    } else {
+        println!("Total matched files: {}", files_processed);
+    }
 
     Ok(())
 }
